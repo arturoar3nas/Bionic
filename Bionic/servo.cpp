1058d00985337a6356fd110e08ad8cc36c10f747
arturoar3nas <arturoar3nas@gmail.com>
2019-06-01 23:57:43 -0400

    add move to Servo class

--- Bionic/servo.cpp
+++ Bionic/servo.cpp
@@ -2,5 +2,13 @@
 
 Servo::Servo()
 {
+    driver = std::make_shared<PCA9685>(0,0); //! bus and addres
+    driver->setPWM(1, 2000);
+}
 
+void Servo::move()
+{
+    driver->setPWMFreq(500);
+    int ret = driver->getPWM(1);
+    std::cout << "pwm: " << std::to_string(ret) << std::endl;
 }
--- Bionic/servo.h
+++ Bionic/servo.h
@@ -1,11 +1,18 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+#include <memory>
+#include <iostream>
+#include "PCA9685.h"
 
 class Servo
 {
 public:
     Servo();
+    void move();
+
+private:
+    std::shared_ptr<PCA9685> driver;
 };
 
 #endif // SERVO_H
