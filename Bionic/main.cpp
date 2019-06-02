d4db6afd1bb2dba96c7c10d4393f19bdca78589e
arturoar3nas <arturoar3nas@gmail.com>
2019-06-01 23:58:00 -0400

    update main function

--- Bionic/main.cpp
+++ Bionic/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-
+#include "hand.h"
 
 int main()
 {
     std::cout << "bionic arm version 1.0.0.1" << std::endl;
+
+    size_t fingers = 1;
+    int joints = 1;
+    Hand hand(fingers, joints);
+    hand.start();
+
+    while(true)
+    {
+        //! check by status hand
+        //! check by battery
+        //! check by another things
+    }
+
     return 0;
 }
